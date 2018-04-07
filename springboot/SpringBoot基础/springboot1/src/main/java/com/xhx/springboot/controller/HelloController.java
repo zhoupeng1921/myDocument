84d6c5efdc2a2d6d15e7798f6b4b3f2ed1978bc6
xuhaixing <haixing1993@163.com>
2018-04-07 23:59:29 +0800

    springboot基础

---
+++ springboot/SpringBoot基础/springboot1/src/main/java/com/xhx/springboot/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.xhx.springboot.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+
+    @RequestMapping(value = "/hello",method = {RequestMethod.GET,RequestMethod.POST})
+    public String index(){
+        return "spring boot my first SpringBoot";
+    }
+
+}
