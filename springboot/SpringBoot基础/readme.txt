a2f141d9d720bd687fb9b942b5b472d415ddabb2
xuhaixing <haixing1993@163.com>
2018-04-08 23:08:09 +0800

    多环境配置

---
+++ springboot/SpringBoot基础/readme.txt
@@ -0,0 +1,2 @@
+springboot1: springboot基本架构,测试test,读取配置文件值
+springboot2: springboot自定义配置文件属性值注入
---
+++ springboot/SpringBoot基础/springboot3/src/main/java/com/xhx/springboot/config/testProfile.java
@@ -0,0 +1,26 @@
+package com.xhx.springboot.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import sun.tools.jar.CommandLine;
+
+/**
+ * made by xuhaixing
+ * 18-4-8 下午10:46
+ */
+@Configuration
+public class testProfile {
+
+    @Value("${server.port}")
+    private String port;
+
+
+    @Profile("!dev") //可以用取反操作符,也可以用多个参数
+    @Bean
+    public CommandLine testPort(){
+        System.out.println("--------------"+port);
+        return new CommandLine();
+    }
+}
