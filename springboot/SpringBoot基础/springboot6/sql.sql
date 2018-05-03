fda089663b8c289dada2fa295f649ac790015b9d
xuhaixing <haixing1993@163.com>
2018-05-03 08:32:56 +0800

    springboot mybatis

---
+++ springboot/SpringBoot基础/springboot6/sql.sql
@@ -0,0 +1,6 @@
+CREATE TABLE `account` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `money` double DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8
---
+++ springboot/SpringBoot基础/springboot6/src/main/java/com/xhx/springboot/service/AccountService.java
@@ -0,0 +1,35 @@
+package com.xhx.springboot.service;
+
+import com.xhx.springboot.mapper.AccountMapper;
+import com.xhx.springboot.entity.Account;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccountService {
+
+    @Autowired
+    private AccountMapper accountMapper;
+
+    public int add(Account account) {
+        return accountMapper.add(account.getName(),account.getMoney());
+    }
+
+    public int update(Account account) {
+        return accountMapper.update(account.getName(),account.getMoney(),account.getId());
+    }
+
+    public int delete(int id) {
+        return accountMapper.delete(id);
+    }
+
+    public Account findById(int id) {
+        return accountMapper.findAccount(id);
+    }
+
+    public List<Account> findList() {
+        return  accountMapper.findAccountList();
+    }
+}
