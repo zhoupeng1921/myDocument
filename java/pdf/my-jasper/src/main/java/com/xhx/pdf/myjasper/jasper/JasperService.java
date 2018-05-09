a2507d2fb0c469bd3a5ca6b0d61e2e1b3eac4c26
xuhaixing <xuhaixing@cnpc.com.cn>
2018-05-09 09:12:13 +0800

    jasper生产pdf

--- java/pdf/my-jasper/src/main/java/com/xhx/pdf/myjasper/jasper/JasperService.java
+++ java/pdf/my-jasper/src/main/java/com/xhx/pdf/myjasper/jasper/JasperService.java
@@ -1,6 +1,7 @@
 package com.xhx.pdf.myjasper.jasper;
 
-import net.sf.jasperreports.engine.JasperRunManager;
+import net.sf.jasperreports.engine.*;
+import net.sf.jasperreports.engine.export.JRPdfExporter;
 import org.springframework.stereotype.Service;
 
 import java.io.File;
@@ -15,15 +16,41 @@ import java.util.HashMap;
 public class JasperService {
 
     public void testPDF() throws Exception {
-        File fileName = new File("E:/ireport/report1.jasper");
-
         HashMap<String, Object> parameters = new HashMap<String, Object>();
-        parameters.put("name", "xuhaixing");
-        byte[] bytes = JasperRunManager.runReportToPdf(fileName.getPath(), parameters);
+        parameters.put("name", "徐海兴");
+        parameters.put("ORIORGNAME", "原组织名称原组织名称原组织名称原组织名称原组织名称");
+        parameters.put("TOORGNAME", "目的组织名称目的组织名称目的组织名称目的组织名称");
+
+
+        File fileName = new File("E:\\ireport\\report1.jasper");
+
 
         FileOutputStream outputStream = new FileOutputStream(new File("d:/pdf.pdf"));
-        outputStream.write(bytes);
-        outputStream.flush();
-        outputStream.close();
+
+    //   byte[] bytes = JasperRunManager.runReportToPdf(fileName.getPath(), parameters);
+
+       JasperPrint jasperPrint = JasperFillManager.fillReport(fileName.getPath(), parameters);
+        JasperExportManager.exportReportToPdfStream(jasperPrint,outputStream);
+
+      //  outputStream.write(bytes);
+      //  outputStream.flush();
+      //  outputStream.close();
+
+
+
+
+        File reportFile = new File("E:\\ireport\\report1.jrxml");
+        JasperReport report = JasperCompileManager.compileReport(reportFile.getPath());
+        JasperPrint jasperPrint2 = JasperFillManager.fillReport(report, parameters);
+        JRPdfExporter exporter = new JRPdfExporter();
+        exporter.setParameter(JRExporterParameter.JASPER_PRINT, jasperPrint2);
+        exporter.setParameter(JRExporterParameter.OUTPUT_STREAM,
+                new FileOutputStream("d:/pdf3.pdf"));
+        exporter.exportReport();// 导出
+
+
+
+
+
     }
 }
