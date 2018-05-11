f8148c28049a96fd5cd7d75104850301ba6b463e
xuhaixing <haixing1993@163.com>
2018-05-11 10:37:04 +0800

    upload file

---
+++ springboot/SpringBoot基础/springboot13/src/main/java/com/xhx/springboot/Controller/FileUploadController.java
@@ -0,0 +1,12 @@
+package com.xhx.springboot.Controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "upload")
+public class FileUploadController {
+
+
+}
