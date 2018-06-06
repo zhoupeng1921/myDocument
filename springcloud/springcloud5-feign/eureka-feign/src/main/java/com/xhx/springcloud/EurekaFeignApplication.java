ded04b7507a85e1e96dff09e293fae0aa1f74a36
xuhaixing <xuhaixing@cnpc.com.cn>
2018-06-06 09:19:24 +0800

    springcloud feign

---
+++ springcloud/springcloud5-feign/eureka-feign/src/main/java/com/xhx/springcloud/EurekaFeignApplication.java
@@ -0,0 +1,16 @@
+package com.xhx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients   //启用feign
+public class EurekaFeignApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaFeignApplication.class, args);
+    }
+}
---
+++ springcloud/springcloud5-feign/eureka-feign/src/main/java/com/xhx/springcloud/controller/RibbionController.java
@@ -0,0 +1,25 @@
+package com.xhx.springcloud.controller;
+
+import com.netflix.discovery.converters.Auto;
+import com.xhx.springcloud.api.HelloFeign;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * xuhaixing
+ * 2018/6/3 16:27
+ */
+@RestController
+public class RibbionController {
+
+    @Autowired
+    private HelloFeign helloFeign;
+
+    @RequestMapping(value = "getName")
+    public String getName(String name){
+        return helloFeign.getName(name);
+    }
+}
+
