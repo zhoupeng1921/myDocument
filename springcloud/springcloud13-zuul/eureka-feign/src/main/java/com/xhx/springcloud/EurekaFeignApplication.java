93c598a05cf0b3766488ddfd306c03e0b7974a36
xuhaixing <haixing1993@163.com>
2018-06-12 23:20:54 +0800

    springcloud zuul filter过滤器

---
+++ springcloud/springcloud13-zuul/eureka-feign/src/main/java/com/xhx/springcloud/EurekaFeignApplication.java
@@ -0,0 +1,18 @@
+package com.xhx.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients   //启用feign
+public class EurekaFeignApplication {
+
+    public static void main(String[] args) {
+        new SpringApplicationBuilder(EurekaFeignApplication.class).web(WebApplicationType.SERVLET).run(args);
+    }
+}
