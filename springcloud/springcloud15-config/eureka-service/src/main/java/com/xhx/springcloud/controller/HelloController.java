cc8c020edae1bf3f287236cca25c6ee5fe41c9a6
xuhaixing <xuhaixing@cnpc.com.cn>
2018-06-14 20:59:10 +0800

    消息总线bus

---
+++ springcloud/springcloud15-config/eureka-service/src/main/java/com/xhx/springcloud/controller/HelloController.java
@@ -0,0 +1,25 @@
+package com.xhx.springcloud.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+/**
+ * xuhaixing
+ * 2018/6/3 16:18
+ */
+@RestController
+@RequestMapping(value = "hello")
+public class HelloController {
+
+    @Value("${user.name}")
+    private String name;
+
+    @RequestMapping(value = "getName")
+    public String getName(){
+        return name;
+    }
+}
