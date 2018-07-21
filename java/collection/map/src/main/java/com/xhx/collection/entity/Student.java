23a1a2619476775a7e352fee7b93cbc4d6ca22c0
xuhaixing <xuhxm@yonyou.com>
2018-07-21 16:57:33 +0800

    'map集合方法'

---
+++ java/collection/map/src/main/java/com/xhx/collection/entity/Student.java
@@ -0,0 +1,50 @@
+package com.xhx.collection.entity;/*
+ * Administrator
+ * 2018/7/21  15:43
+ *
+ */
+
+public class Student {
+    private String name;
+    private String sex;
+    private double money;
+
+    public Student(String name, String sex, double money) {
+        this.name = name;
+        this.sex = sex;
+        this.money = money;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSex() {
+        return sex;
+    }
+
+    public void setSex(String sex) {
+        this.sex = sex;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(double money) {
+        this.money = money;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", sex='" + sex + '\'' +
+                ", money=" + money +
+                '}';
+    }
+}
--- java/collection/map/src/test/java/com/xhx/collection/TestHashMapApp.java
+++ java/collection/map/src/test/java/com/xhx/collection/TestHashMapApp.java
@@ -1,11 +1,9 @@
 package com.xhx.collection;
 
+import com.xhx.collection.entity.Student;
 import org.junit.Test;
 
-import java.util.HashMap;
-import java.util.Hashtable;
-import java.util.Iterator;
-import java.util.Map;
+import java.util.*;
 
 /**
  * xuhaixing
@@ -49,10 +47,89 @@ public class TestHashMapApp {
 
     }
 
+    /**
+     * HashMap与HashTable不同
+     */
     public void testHashMapAndHashTable(){
         HashMap<String,Object> map = new HashMap();
         Hashtable<String,Object> hashtable = new Hashtable();
     }
-}
 
+
+    /**
+     * compute(K key, BiFunction<? super K, ? super V, ? extends V> remappingFunction) {
+     *         Objects.requireNonNull(remappingFunction);
+     *         V oldValue = get(key);
+     *
+     *   只要新value不为null就会插入新key或者覆盖原来value,如果新value为null,就会把这个键值对直接删除
+     */
+    @Test
+    public void testHashMapCompute(){
+        List<Student> studentList = Arrays.asList(
+                new Student("apple",null,10),
+                //new Student("orange","女",30),
+              //  new Student("pink",null,60),
+                new Student("pink","男",60),
+                new Student("black","男",60),
+                new Student("orange",null,150)
+        );
+        Map<String,String> map = new HashMap<String, String>();
+        map.put("pink",null);//原来为空，测试现在不为空
+        map.put("orange","女");//原来不为空，测试现在为空
+        map.put("apple",null);//原来为空，测试现在为空
+        studentList.stream().forEachOrdered(student->map.compute(student.getName(),(k,v)->{
+            return student.getSex();}));
+        System.out.println(map);
+        // {pink=男, black=男}
+    }
+
+    /**
+     * computeIfAbsent(K key, Function<? super K, ? extends V> mappingFunction) {
+     *         Objects.requireNonNull(mappingFunction);
+     *         返回值就是Function里面的V，所以computeIfAbsent方法执行后还可以继续做一系列的操作,
+     *         但是value已经放到集合中了，所以只有引用类型才可以继续操作，String不行
+     *
+     * 只要是原来的value为null,就会插入或者更新，如果不为null，不变
+     * 如果新的value为null,则不做操作
+     *
+     */
+    @Test
+    public void testHashMapComputeIfAbsent(){
+        List<Student> studentList = Arrays.asList(
+                new Student("apple",null,10),
+                //new Student("orange","女",30),
+                //  new Student("pink",null,60),
+                new Student("pink","男",60),
+                new Student("black","男",60),
+                new Student("orange",null,150)
+        );
+        Map<String,String> map = new HashMap<String, String>();
+        map.put("pink",null);//原来为空，测试现在不为空
+        map.put("orange","女");//原来不为空，测试现在不为空
+        studentList.stream().forEachOrdered(student->map.computeIfAbsent(student.getName(),k->k).toUpperCase());
+        System.out.println(map);
+        // {{orange=女, apple=apple, pink=pink, black=black}   toUpperCase没有达到效果
+    }
+
+    @Test
+    public void testHashMapComputeIfAbsent2(){
+        List<Student> studentList = Arrays.asList(
+                new Student("apple",null,10),
+                new Student("pink","男",60),
+                new Student("black","男",60),
+                new Student("black","女",80)
+        );
+        Map<String,List<Student>> map = new HashMap<>();
+        studentList.stream().forEachOrdered(
+                student->map.computeIfAbsent(student.getName(),k->new ArrayList<Student>()).add(student)
+        );
+        System.out.println(map);
+        // {
+        // apple=[Student{name='apple', sex='null', money=10.0}],
+        // pink=[Student{name='pink', sex='男', money=60.0}],
+        // black=[Student{name='black', sex='男', money=60.0}, Student{name='black', sex='女', money=80.0}]
+        // }   add达到效果了
+    }
+
+}
 
