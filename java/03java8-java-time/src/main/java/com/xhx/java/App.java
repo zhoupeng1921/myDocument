d696f35adf6a319be118697b5a8131c5bfb2a20b
xuhaixing <haixing1993@163.com>
2018-10-08 16:50:04 +0800

    java8 java.time类

---
+++ java/03java8-java-time/src/main/java/com/xhx/java/App.java
@@ -0,0 +1,13 @@
+package com.xhx.java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
