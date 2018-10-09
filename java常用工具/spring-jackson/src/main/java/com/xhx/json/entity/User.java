a1f5b4786cbd358604764e4472fb9864e92cea2c
xuhaixing <haixing1993@163.com>
2018-10-09 22:53:03 +0800

    jackson常用注解

---
+++ java常用工具/spring-jackson/src/main/java/com/xhx/json/entity/User.java
@@ -0,0 +1,113 @@
+package com.xhx.json.entity;
+
+import com.fasterxml.jackson.annotation.*;
+
+import java.time.LocalDate;
+import java.util.Date;
+
+/**
+ * xuhaixing
+ * 2018/10/9 21:29
+ **/
+
+/**
+ * @JsonInclude  value某些属性是否序列化的规则
+ * @JsonRootName  需要设置mapper.enable(SerializationFeature.WRAP_ROOT_VALUE); 否则rootName无效
+ */
+@JsonInclude(value = JsonInclude.Include.NON_EMPTY)
+@JsonRootName(value = "root")
+public class User {
+    private String id;
+
+    /**
+     * @JsonProperty
+     * value 可以改变序列化和反序列化时属性的名称
+     * access 控制是否可以被序列号或者反序列化
+     */
+//    @JsonProperty(value = "fullName",access = JsonProperty.Access.READ_ONLY)
+    @JsonProperty(value = "fullName")
+    private String name;
+    private Integer age;
+    /**
+     *  @JsonIgnore 忽略这个属性不转换
+     */
+    @JsonIgnore
+    private LocalDate birthday;
+    private Double money;
+
+    private String compute;
+
+    /**
+     * @JsonFormat pattern做格式转换
+     */
+    @JsonFormat(pattern = "yyyy年MM月dd日 HH:mm:ss",timezone ="GMT+8")
+    private Date date;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public LocalDate getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(LocalDate birthday) {
+        this.birthday = birthday;
+    }
+
+    public Double getMoney() {
+        return money;
+    }
+
+    public void setMoney(Double money) {
+        this.money = money;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public String getCompute() {
+        return compute;
+    }
+
+    public void setCompute(String compute) {
+        this.compute = compute;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", age=" + age +
+                ", birthday=" + birthday +
+                ", money=" + money +
+                ", date=" + date +
+                '}';
+    }
+}
---
+++ java常用工具/spring-jackson/src/test/java/com/xhx/json/AppTest.java
@@ -0,0 +1,72 @@
+package com.xhx.json;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.xhx.json.entity.User;
+import org.junit.Test;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+    private User user;
+    {
+        user = new User();
+        user.setId("111");
+        user.setName("xuhaixing");
+        user.setBirthday(LocalDate.of(1993,8,18));
+        user.setAge(25);
+        user.setMoney(100000.0);
+        user.setDate(new Date());
+    }
+
+    ObjectMapper mapper = new ObjectMapper();
+    {
+        //出现未知属性时不报错
+        mapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    }
+
+    /**
+     * @JsonIgnore 修饰的属性不会进行json操作
+     */
+    @Test
+    public void testJson1() throws Exception{
+
+        mapper.enable(SerializationFeature.WRAP_ROOT_VALUE);
+        String str = mapper.writeValueAsString(user);
+
+        //还可以输出到文件、流、字节等
+        System.out.println(str);
+        //{"id":"111","name":"xuhaixing","age":25,"money":100000.0,"date":1539092674111}
+
+        User user = mapper.readValue(str, User.class);
+        System.out.println(user);
+        //User{id='111', name='xuhaixing', age=25, birthday=null, money=100000.0, date=Tue Oct 09 21:44:34 CST 2018}
+    }
+
+    /**
+     * 数组的序列号与反序列化
+     * @throws Exception
+     */
+    @Test
+     public void testJson2() throws Exception{
+        List<User> users = new ArrayList<>();
+        users.add(user);
+
+        String s = mapper.writeValueAsString(users);
+        System.out.println(s);
+        //[{"id":"111","age":25,"money":100000.0,"date":"2018年10月09日","fullName":"xuhaixing"}]
+
+        List<User> userList = mapper.readValue(s, new TypeReference<List<User>>() {
+        });
+        System.out.println(userList);
+        //[User{id='111', name='xuhaixing', age=25, birthday=null, money=100000.0, date=Tue Oct 09 08:00:00 CST 2018}]
+    }
+}
