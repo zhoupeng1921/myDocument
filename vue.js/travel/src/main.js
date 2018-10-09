6a9fb898cb80cc927caf31232a0010c1b538fc10
xuhaixing <haixing1993@163.com>
2018-10-09 19:12:56 +0800

    travel

--- vue.js/travel/src/main.js
+++ vue.js/travel/src/main.js
@@ -4,6 +4,8 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import fastClick from 'fastclick'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 // 重置样式
 import './assets/styles/reset.css'
 import './assets/styles/border.css'
@@ -12,6 +14,8 @@ import './assets/styles/iconfont.css'
 Vue.config.productionTip = false
 // 移动端300ms点击延迟
 fastClick.attach(document.body)
+
+Vue.use(VueAwesomeSwiper /* { default global options } */)
 
 /* eslint-disable no-new */
 new Vue({
---
+++ vue.js/travel/src/pages/home/components/Swiper.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <!-- slides -->
+      <swiper-slide>
+        <img class="swiper-img" src="http://img1.qunarzz.com/piao/fusion/1809/91/63a7f117b30cd202.jpg_750x200_6c2dcbc9.jpg"/>
+    </swiper-slide>
+        <swiper-slide>
+          <img class="swiper-img"  src="http://img1.qunarzz.com/piao/fusion/1809/31/da037478f37cf202.jpg_750x200_a02cf862.jpg"/>
+    </swiper-slide>
+          <!-- Optional controls -->
+          <div class="swiper-pagination"
+               slot="pagination">
+
+          </div>
+          <!-- <div class="swiper-button-prev" slot="button-prev"></div>
+    <div class="swiper-button-next" slot="button-next"></div> -->
+          <!-- <div class="swiper-scrollbar"   slot="scrollbar"></div> -->
+    </swiper>
+  </div>
+</template>
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination'
+      }
+    }
+  }
+}
+</script>
+<style lang="stylus" scoped>
+.wrapper
+  width 100%
+height 0
+  overflow hidden
+  padding-bottom 31.25%
+  background #eee
+.swiper-img
+  width 100%
+</style>
