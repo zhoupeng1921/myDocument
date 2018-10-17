62bfa83286b2b7c2f5b6afeac3460b2208a13683
xuhaixing <haixing1993@163.com>
2018-10-17 22:13:40 +0800

    npm使用

---
+++ nodejs/02/05npm.js
@@ -0,0 +1,18 @@
+// npm install art-template --save  --save会把包保存到package.json文件中
+// npm init 会初始化这个文件  -y跳过向导
+// npm install 把依赖项重新导入  简写 npm i -S
+// npm install --global npm 升级npm
+// npm uninstall 包名 --save  简写 npm un -S 包名
+// npm xxx help 帮助
+
+//安装cnpm,
+// npm install --global cnpm
+
+//用npm使用taobao镜像服务器
+//npm install jquery https://registry.npm.taobao.org
+
+//修改npm镜像地址
+//npm config set registry https://registry.npm.taobao.org
+
+//查看npm配置信息
+//npm config list
