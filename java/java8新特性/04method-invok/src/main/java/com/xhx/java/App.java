47a7a23deadb5c8ed65c83be35c8876fb206c709
xuhaixing <haixing1993@163.com>
2018-11-02 00:07:19 +0800

    java8 方法引用总结

--- java/java8新特性/04method-invok/src/main/java/com/xhx/java/App.java
+++ java/java8新特性/04method-invok/src/main/java/com/xhx/java/App.java
@@ -10,6 +10,8 @@ public class App {
 
     /**
      * 1.构造器引用
+     * ClassName::New 相当于 new ClassName()
+     * 如果调用时有参数，相当于调用带参的构造函数
      */
     @Test
     public void test1(){
@@ -22,6 +24,8 @@ public class App {
 
     /**
      * 2.静态方法引用
+     * CLassName::static_method =》 CLassName.static_method()
+     * 如果调用时有参数，直接放到静态方法的形参中
      */
     @Test
     public void test2(){
@@ -30,6 +34,9 @@ public class App {
 
     /**
      * 特定类对方法引用
+     * ClassName::instance_method => obj.instance_method(xxx)
+     * 调用时参数：第一个参数是实体对象 上面的obj
+     * 如果有其他参数，会传到方法中
      */
     @Test
     public void test3(){
@@ -42,6 +49,8 @@ public class App {
 
     /**
      * 特定对象对方法引用
+     * instance::instance_method =》instance.instance_method(xxxx);
+     * 如果有参数，参数就是方法的参数
      */
     @Test
     public void test4(){
@@ -49,6 +58,13 @@ public class App {
         user.setId("123");
         user.setAge(13);
         user.setName("beaufulGirl");
+        toString(user::printString);
         
+    }
+    public interface MyInterface{
+        void doSomething();
+    }
+    public void toString(MyInterface face){
+        face.doSomething();
     }
 }
