02e29fee81dcf5e3c60a0ec3d35dd8615102ee5b
xuhaixing <haixing1993@163.com>
2018-12-05 23:16:31 +0800

    Deque

---
+++ java/02collection/deque/src/test/java/com/xhx/java/TestDeque.java
@@ -0,0 +1,12 @@
+package com.xhx.java;
+
+
+import java.util.Deque;
+import java.util.Stack;
+
+public class TestDeque {
+
+    public void test01(){
+        Deque
+    }
+}
---
+++ java/02collection/笔记/ArrayDeque.md
@@ -0,0 +1,7 @@
+# ArrayDeque
+
+## 介绍
+
+1. ArrayDeque是可扩容的
+2. 不是线程安全的
+3. 做栈（Stack）时性能比`Stack`好，做队列（Queue）时性能比`LinkedList`好
