0e9234ec4139832ed0ebed2323226682f5ec9da3
xuhaixing <haixing1993@163.com>
2018-12-08 09:25:23 +0800

    ArrayDeque

--- java/02collection/deque/src/test/java/com/xhx/java/TestArrayDeque.java
+++ java/02collection/deque/src/test/java/com/xhx/java/TestArrayDeque.java
@@ -5,14 +5,31 @@ import org.junit.Test;
 
 import java.util.ArrayDeque;
 
+/**
+ * 方法详情看笔记
+ */
 public class TestArrayDeque {
 
     @Test
     public void test01(){
         ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
         arrayDeque.addFirst(1);
-        arrayDeque.addFirst(2);
-        arrayDeque.addFirst(3);
+        //offer有返回值
+        boolean b1 = arrayDeque.offerFirst(2);
+
+        arrayDeque.addLast(3);
+        boolean b2 = arrayDeque.offerLast(4);
+
+        //remove空会抛异常
+        Integer integer = arrayDeque.removeFirst();
+        Integer integer1 = arrayDeque.removeLast();
+
+
+        //pool空时，返回null
+        Integer first = arrayDeque.pollFirst();
+        Integer integer2 = arrayDeque.pollLast();
+
+
 
     }
 }
