c57b8b07ec1b1cb335a043112837244722a31c95
xuhaixing <haixing1993@163.com>
2018-12-09 22:10:32 +0800

    修改ActiveMQ

--- springboot/SpringBoot基础/springboot11/mq1-springboot/src/main/java/springboot/mq/Consumer.java
+++ springboot/SpringBoot基础/springboot11/mq1-springboot/src/main/java/springboot/mq/Consumer.java
@@ -3,6 +3,10 @@ package springboot.mq;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
+import javax.jms.JMSException;
+import javax.jms.Session;
+import javax.jms.TextMessage;
+
 @Component
 public class Consumer {
 
@@ -12,15 +16,27 @@ public class Consumer {
      * @param message
      */
     @JmsListener(destination = "Q_TOPIC_USERIDENTITY")
-    public void receiveTopic(String message){
-        System.out.println("收到的TOPIC消息:"+message);
+    public void receiveTopic(TextMessage message, Session session) throws JMSException{
+        try {
+            System.out.println("收到的TOPIC消息:"+message.getText());
+            System.out.println(session.getAcknowledgeMode());
+        } catch (JMSException e) {
+            e.printStackTrace();
+            session.recover();
+        }
     }
 
     /**
      * @param message
      */
     @JmsListener(destination = "Q_QUEUE_USERIDENTITY")
-    public void receiveQueue(String message){
-        System.out.println("收到的QUEUE消息:"+message);
+    public void receiveQueue(TextMessage message, Session session) throws JMSException{
+        try {
+            System.out.println("收到的QUEUE消息:"+message.getText());
+            System.out.println(session.getAcknowledgeMode());
+        } catch (JMSException e) {
+            e.printStackTrace();
+            session.recover();
+        }
     }
 }
--- springboot/SpringBoot基础/springboot11/mq1-springboot/src/main/java/springboot/mq/Provider.java
+++ springboot/SpringBoot基础/springboot11/mq1-springboot/src/main/java/springboot/mq/Provider.java
@@ -2,8 +2,10 @@ package springboot.mq;
 
 import org.apache.activemq.command.ActiveMQQueue;
 import org.apache.activemq.command.ActiveMQTopic;
+import org.apache.activemq.pool.PooledConnectionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.core.JmsMessagingTemplate;
+import org.springframework.jms.core.JmsTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
@@ -15,25 +17,27 @@ public class Provider {
     @Autowired
     private JmsMessagingTemplate jmsMessagingTemplate;
 
+    @Autowired
+    private JmsTemplate jmsTemplate;
+
+    @Autowired
+    private PooledConnectionFactory pooledConnectionFactory;
 
 
-
-    @Scheduled(fixedDelay = 5000)//每5秒执行一次
+    //@Scheduled(fixedDelay = 5000)//每5秒执行一次
     public void sendTopic(){
-        String identityCard="132927196512021354";
+        String identityCard="188997196512021354";
         // jmsMessagingTemplate.convertAndSend("Q_USERIDENTITY",identityCard);//默认按照spring配置文件来
-
         ActiveMQTopic destination = new ActiveMQTopic("Q_TOPIC_USERIDENTITY");
         jmsMessagingTemplate.convertAndSend(destination,identityCard);
     }
 
     @Scheduled(fixedDelay = 5000)
     public void sendQueue(){
-        String identityCard="132927196512021354";
-
+        String identityCard="188997196512021354";
 
         ActiveMQQueue destination = new ActiveMQQueue("Q_QUEUE_USERIDENTITY");
-        jmsMessagingTemplate.convertAndSend(destination,identityCard);
+        jmsTemplate.convertAndSend(destination,identityCard);
 
     }
 }
