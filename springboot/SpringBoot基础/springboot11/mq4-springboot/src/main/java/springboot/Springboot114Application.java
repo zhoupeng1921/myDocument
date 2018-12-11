f2a1798b4d69e9f21ad9427d753c1a6a202542db
xuhaixing <haixing1993@163.com>
2018-12-11 22:43:28 +0800

    自定义多个jmsTemplate

---
+++ springboot/SpringBoot基础/springboot11/mq4-springboot/src/main/java/springboot/Springboot114Application.java
@@ -0,0 +1,12 @@
+package springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot114Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot114Application.class, args);
+    }
+}
