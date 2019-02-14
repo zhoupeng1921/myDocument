c5141ad078ca4460b3dd1fe06f59ee59682547fb
xuhaixing <haixing1993@163.com>
2019-02-14 23:02:26 +0800

    elasticsearch

---
+++ elasticsearch/es-01/src/main/java/com/xhx/java/App.java
@@ -0,0 +1,13 @@
+package com.xhx.java;
+
+public class App {
+
+    private static String HOST="192.168.91.150";
+    private static int PORT=9200;
+
+
+    public static void main(String[] args) {
+
+
+    }
+}
