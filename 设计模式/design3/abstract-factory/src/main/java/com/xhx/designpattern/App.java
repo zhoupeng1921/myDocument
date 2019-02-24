11ad6b361bf78659cd4e1102888c6383c1555f34
xuhaixing <haixing1993@163.com>
2019-02-24 17:13:31 +0800

    抽象工厂

--- 设计模式/design3/abstract-factory/src/main/java/com/xhx/designpattern/App.java
+++ 设计模式/design3/abstract-factory/src/main/java/com/xhx/designpattern/App.java
@@ -1,6 +1,11 @@
 package com.xhx.designpattern;
 
-
+/**
+ * 抽象工厂，可以在工厂中直接获取各种类型产品，（工厂类中可以直接看出能生产多少产品）
+ * 解决了工厂方法模式不知道能生产多少类型产品问题，（每个工厂对应一种产品，不知道有多少工厂）
+ *
+ * 结局了编写代码的人与调用代码的人的痛处
+ */
 public class App 
 {
     public static void main( String[] args )
