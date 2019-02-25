6427af81b204e95a75e9a43344d7d316f6d21e85
xuhaixing <haixing1993@163.com>
2019-02-25 21:23:45 +0800

    ThreadLocal

--- java/03java.lang/ThreadLocal/thread-local/src/test/java/com/xhx/java/TestThreadLocal.java
+++ java/03java.lang/ThreadLocal/thread-local/src/test/java/com/xhx/java/TestThreadLocal.java
@@ -9,7 +9,12 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 /**
- * 原理： 内部有一个ThreadLocal.ThreadLocalMap，用Thread.currentThread()当key，每个线程可以维护一个自己的变量对象
+ * 原理： ThreadLocal内部有一个ThreadLocal.ThreadLocalMap  KEY 是ThreadLocal value是值
+ * 每一个Thread内部有一个threadLocals属性，为ThreadLocal.ThreadLocalMap类型，同一个ThreadLocal不同的线程，获取的ThreadLocalMap不一样
+ * ThreadLocal.ThreadLocalMap 里有一个Entry[] table类型的数组
+ * 数组的下标threadLocal.threadLocalHashCode & (table.length - 1)这么得来的，如果冲突会后移
+ *
+ * 注意：如果ThreadLocal设置的值是多线程共享的，依旧有线程安全问题。
  *
  * 应用场景：同一事务下的connection
  */
@@ -55,4 +60,21 @@ public class TestThreadLocal {
             return new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         }
     };
+
+
+    /**
+     * 不同的ThreadLocal，获取的都是Thread.currentThread()的ThreadLocalMap
+     * @throws Exception
+     */
+    @Test
+    public void test03() throws Exception{
+        ThreadLocal<Integer> threadLocal = new ThreadLocal<>();
+        threadLocal.set(100);
+        threadLocal.set(200);
+        ThreadLocal<String> threadLocal2 = new ThreadLocal<>();
+        threadLocal2.set("aaa");
+
+        System.out.println(threadLocal.get());
+        System.out.println(threadLocal2.get());
+    }
 }
