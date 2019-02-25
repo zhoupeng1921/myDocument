b06cf96a849d80e3cab44e1998e2d360702db2c8
xuhaixing <haixing1993@163.com>
2019-02-25 17:11:22 +0800

    ThreadLocal

--- java/03java.lang/ThreadLocal/thread-local/src/test/java/com/xhx/java/TestThreadLocal.java
+++ java/03java.lang/ThreadLocal/thread-local/src/test/java/com/xhx/java/TestThreadLocal.java
@@ -8,7 +8,7 @@ import java.util.List;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
-public class TestThradLocal {
+public class TestThreadLocal {
 
     @Test
     public void test01() throws Exception{
