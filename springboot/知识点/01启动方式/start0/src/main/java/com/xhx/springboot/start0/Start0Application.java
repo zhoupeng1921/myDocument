d2cc6783ff468e5133a659dc6ba4ef05ce5a780c
xuhaixing <haixing1993@163.com>
2019-03-03 20:29:51 +0800

    springboot启动方式

---
+++ springboot/知识点/01启动方式/start0/src/main/java/com/xhx/springboot/start0/Start0Application.java
@@ -0,0 +1,13 @@
+package com.xhx.springboot.start0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Start0Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Start0Application.class, args);
+    }
+
+}
