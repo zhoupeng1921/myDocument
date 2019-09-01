c71f046efa4dfeb53844325fd845002bc9c2c88e
xuhaixing <haixing1993@163.com>
2019-09-01 10:18:52 +0800

    删除jdbcTemplate

--- springboot/SpringBoot基础/readme.md
+++ springboot/SpringBoot基础/readme.md
@@ -1,5 +1,3 @@
-springboot3: 多环境配置 profile
-springboot4: jdbc配置mysql
 springboot5: jpa/spring.datasource详细配置 启动时初始化数据库
 springboot5-2: jpa @Table与@Entity注解
 springboot5-3: jpa @Convert注解 AttributeConverter转换数据库字段类型与实体类数据类型
--- springboot/SpringBoot基础/springboot4/sql.sql
+++
@@ -1,6 +0,0 @@
- CREATE TABLE `account` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `name` varchar(20) NOT NULL,
-  `money` double DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8
